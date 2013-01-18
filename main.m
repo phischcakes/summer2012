9bedb5232d9007bb382cfde5f345c40946d05f2d
Brandon Kinard <kinard97.brandon@gmail.com>
2013-01-18 08:54:14 -1000

    Adding Documents
    Needs Organization

---
+++ main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Are You A WIZARD?
+//
+//  Created by Brandon Kinard on 8/26/12.
+//  Copyright (c) 2012 Brandon Kinard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+      
+        
+    }
+    return 0;
+}
+
---
+++ program1.h
@@ -0,0 +1,13 @@
+//
+//  program1.h
+//  
+//
+//  Created by Brandon Kinard on 8/26/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface program1 : NSObject
+
+@end
